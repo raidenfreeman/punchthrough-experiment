db98a9793839987d593455829802d107b32515d5
Raiden Freeman <raidenfreeman@hotmail.com>
2019-01-06 15:09:32 +0200

    Removed error handler

--- index.js
+++ index.js
@@ -37,9 +37,9 @@ app.use(session({
     saveUninitialized: false
 }));
 
-if (!isProduction) {
-    app.use(errorHandler());
-}
+// if (!isProduction) {
+//     app.use(errorHandler());
+// }
 
 //Configure Mongoose
 // mongoose.connect(mongodUri, {
