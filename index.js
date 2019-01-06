465fd53d74574efc28345d5a43a9c3f947e97451
Raiden Freeman <raidenfreeman@hotmail.com>
2019-01-06 03:13:37 +0200

    Updated to use a port from heroku

--- index.js
+++ index.js
@@ -5,16 +5,22 @@ const client = dgram.createSocket('udp4');
 
 const tarIP = '141.237.113.93';
 let i = 0;
+console.log('Trying to send on port:', process.env.PORT);
 const int = setInterval(() => {
     i++;
     if (i > 1000) {
         clearInterval(int);
         client.close();
+        console.log('STOPPING!');
     } else {
-        client.send(Buffer.from('Rand ' + i + ' bytes!'), 37888, tarIP, (err) => {
+        const m = 'Rand ' + i + ' bytes!';
+        client.send(Buffer.from(m), process.env.PORT, tarIP, (err) => {
             if (err) {
                 clearInterval(int);
                 client.close();
+                console.log(err);
+            } else {
+                console.log('sent:   ', m);
             }
         });
     }
